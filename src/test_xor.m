0bdfae72a8cbe2cbb31818be7fe35a5c1cd78948
Victoria <mvictoria.cavo@gmail.com>
2016-05-07 14:03:20 -0300

    Add testing script for multilayer network learning

---
+++ src/test_xor.m
@@ -0,0 +1,21 @@
+% SCRIPT TO TEST MULTILAYER NETWORK
+
+function ret = test_xor()
+	% initialize net
+	nett = initialize_mutilayer_network([2 4 1],-0.5,0.5);
+	% XOR training set
+	t{1} = [ 0 0 ; 0 1 ; 1 0 ; 1 1 ];
+	t{2} = [ 0 ; 1 ; 1 ; 0 ];
+	% Learn
+	net = multilayer_perceptron_online(nett,t,0.01,@tanh_ft,@tanh_ft_der,0.5,1);
+	% Check
+	printf("Checking XOR with: b=1, g=tanh, err=0.01, n=0.5\n");
+	get_network_output(net, t{1}, @tanh_ft,1) 
+
+	% Learn
+	net = multilayer_perceptron_online(nett,t,0.01,@exp_ft,@exp_ft_der,0.5,1);
+	% Check
+	printf("Checking XOR with: b=1, g=exp, err=0.01, n=0.5\n");
+	get_network_output(net, t{1}, @exp_ft,1) 
+
+end
