254224061fa59df8dc820b7d9a66b4ce915b5f5b
Victoria <mvictoria.cavo@gmail.com>
2016-05-10 12:46:42 -0300

    Test adaptative etha

--- src/test_xor.m
+++ src/test_xor.m
@@ -2,19 +2,13 @@
 
 function ret = test_xor()
 	% initialize net
-	nett = initialize_mutilayer_network([2 4 1],-0.5,0.5);
+	nett = initialize_mutilayer_network([3 5 2 1],-0.5,0.5);
 	% XOR training set
-	t = get_training_set(2, @xor);
+	t = get_training_set(3, @xor);
 	% Learn
-	net = multilayer_perceptron_online(nett,t,0.01,@tanh_ft,@tanh_ft_der,0.5,1);
+	net = multilayer_perceptron_batch_adaptative_etha(nett,t,0.1,@tanh_ft,@tanh_ft_der,1,0.5,0.9,0.4,0.05);
 	% Check
-	printf("Checking XOR with: b=1, g=tanh, err=0.01, n=0.5\n");
+	printf("Checking XOR with: betha=1, g=tanh, err=0.1, n=0.5, alpha=0.9, a=0.5, b=0.05\n");
 	get_network_output(net, t{1}, @tanh_ft,1) 
-
-	% Learn
-	net = multilayer_perceptron_online(nett,t,0.01,@exp_ft,@exp_ft_der,0.5,1);
-	% Check
-	printf("Checking XOR with: b=1, g=exp, err=0.01, n=0.5\n");
-	get_network_output(net, t{1}, @exp_ft,1) 
 
 end
