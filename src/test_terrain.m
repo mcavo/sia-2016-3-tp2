c27fc46100c6641cd17157e3eeb4a3727a8304d8
Victoria <mvictoria.cavo@gmail.com>
2016-05-16 20:08:17 -0300

    Test terrain example -> WORKING

--- src/test_terrain.m
+++ src/test_terrain.m
@@ -1,5 +1,27 @@
-terr = load("../data/terrain5.txt")
-x = terr(:,1)';
-y = terr(:,2)';
-z = terr(:,3)';
-plot3(x,y,z,'.')
+function net = test_terrain()
+	terr = load("../data/terrain5.txt");
+	[M terr_n] = normalize_terrain(1,terr);
+
+	ttrain = load("../data/input/mitad1.txt");
+
+	t{1} = terr(:,1:2);
+	t{2} = terr(:,3);
+
+	nett = initialize_mutilayer_network([2 5 2 1],-0.5,0.5);
+
+	err=0.001; g=@tanh_ft; g_der=@tanh_ft_der; betha=0.5; n=0.5; alpha=0; a=0; b=0;
+
+	net = multilayer_perceptron_online_adaptive_etha(nett,t,err,g,g_der,betha,n,alpha,a,b);
+	o = feedfoward(net, t{1}, g, betha);
+	O = o{size(net)(2)};
+	terr_out = denormalize_terrain(1,O,M);
+
+	figure(3)
+	hold on
+	plot3(terr(:,1),terr(:,2),terr(:,3),'r.')
+	plot3(terr(:,1),terr(:,2),terr_out,'g.')
+	hold off
+
+	figure (4)
+	plot(1:length(terr_out),(terr(:,3)-terr_out).^2,'p');
+end
