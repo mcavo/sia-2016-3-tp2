4323cd1bd681cd93cc197d0a7bd41e058aa23216
Victoria <mvictoria.cavo@gmail.com>
2016-05-21 10:46:07 -0300

    b -> betha

--- src/ann_terrain/backpropagation_online.m
+++ src/ann_terrain/backpropagation_online.m
@@ -1,6 +1,6 @@
 function [retnet,deltaW] = backpropagation_online (net,V,g_der,betha,deltaW)
 
-	delta{M} = g_der(b,V_k{M}).*(t{2}(vec(k),:)-V_k{M});
+	delta{M} = g_der(betha,V_k{M}).*(t{2}(vec(k),:)-V_k{M});
 
 	for m=M:-1:2
 		delta{m-1} = g_der(betha,V_k{m-1}).*(delta{m}*(net{m}(2:end,:))');
