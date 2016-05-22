f93f2d5393466106b0f0d0a037147ae5fe7c85c7
Victoria <mvictoria.cavo@gmail.com>
2016-05-22 18:16:34 -0300

    Add legend and correct terrain plotting

--- src/ann_terrain/plotDiff.m
+++ src/ann_terrain/plotDiff.m
@@ -1,17 +1,18 @@
 o1 = feedfoward(net, t{1}, g, betha);
-O1 = o1{size(net)(2)}
+O1 = o1{size(net)(2)};
 terr_out1 = denormalize_terrain(1,O1,M(3,:));
 o2 = feedfoward(net, t2{1}, g, betha);
-O2 = o2{size(net)(2)}
+O2 = o2{size(net)(2)};
 terr_out2 = denormalize_terrain(1,O2,M(3,:));
 
 figure(5)
 hold on
 plot3(terr(:,1),terr(:,2),terr(:,3),'r.')
 legend('Terrain');
-plot3(terr(:,1),terr(:,2),terr_out1,'g.','DisplayName','Training set');
-plot3(terr(:,1),terr(:,2),terr_out2,'b.','DisplayName','Testing set');
+plot3(terr1(:,1),terr1(:,2),terr_out1,'g.','DisplayName','Training set');
+plot3(terr2(:,1),terr2(:,2),terr_out2,'b.','DisplayName','Testing set');
+title('Terrain representation', 'fontsize', 20, 'fontname', 'avenir next');
+xlabel('x1', 'fontsize', 15, 'fontname', 'avenir next');
+ylabel('x2', 'fontsize', 15, 'fontname', 'avenir next');
+zlabel('z', 'fontsize', 15, 'fontname', 'avenir next');
 hold off
-
-figure (6)
-plot(1:length(terr_out),(terr(:,3)-terr_out).^2,'p');
